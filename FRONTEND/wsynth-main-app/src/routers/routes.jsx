c5ce47cdfa1b6c277ef693d92898bbf7a3a01483
michaeelespinoza <mike.omar168@gmail.com>
2023-04-02 23:51:21 +0200

    Frontend Commit

---
+++ FRONTEND/wsynth-main-app/src/routers/routes.jsx
@@ -0,0 +1,11 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import {Home} from "../pages/Home";
+
+export function MyRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+        </Routes>
+    );
+}
